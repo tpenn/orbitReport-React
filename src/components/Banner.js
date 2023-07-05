6d6eca3c8576a6f1f40695a801f0db709a711d16
Terri <404516+tpenn@users.noreply.github.com>
2023-07-05 07:54:47 -0500

    Added use of optional styling.css

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,10 @@
+import './styling.css';
+
 export default function Banner() {
     return (
-        <h1>Orbit Report</h1>
+        <header>
+            <h1>Orbit Report</h1>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </header>
     );
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,8 @@
+import './styling.css';
+
 const Buttons = () => {
   return (
-    <div>
+    <div className="flex-container">
       <button>Placeholder Button</button>
       <button>All Orbits</button>
       </div>
