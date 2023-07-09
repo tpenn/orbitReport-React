f518f61070932dfac8f3b2b72f375ccbda25585a
Terri <404516+tpenn@users.noreply.github.com>
2023-07-09 07:10:19 -0500

    Switch from using props in function argument to destructuring like everywhere else

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 import satData from './satData';
 
-const Buttons = (props) => {
+const Buttons = ({ filterByType, displaySats, setSat }) => {
   const [active, setActive] = useState('All');
-  const handleClick = event => { setActive(event.target.id); props.filterByType(event.target.id); };
+  const handleClick = event => { setActive(event.target.id); filterByType(event.target.id); };
 
   return (
     <div className="flex-container">
-      {props.displaySats.map((orbitType, idx) =>
+      { displaySats.map((orbitType, idx) =>
         (
           <button onClick={handleClick} key={idx} id={orbitType} className={active === orbitType ? "active" : undefined}>
             {orbitType} Orbit
           </button>
         )
       )}
-      <button key="All" id="All" onClick={_ => { setActive('All'); props.setSat(satData); }} className={active === "All"  ? "active" : undefined}>
+      <button key="All" id="All" onClick={_ => { setActive('All'); setSat(satData); }} className={active === "All"  ? "active" : undefined}>
         All Orbits
       </button>
       </div>
